5ed3ea4bb9c84bf3609b1f968cad342b2456fd83
dayton-will <willd1799@student.faytechcc.edu>
2025-08-20 15:38:03 +0000

    Assignment 8/20/25

---
+++ m1t1_hello.cpp
@@ -0,0 +1,16 @@
+/*
+CSC 134
+M1T1 - Hello World
+D Will
+8/20/25
+First Program
+*/
+// magic words
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<< "Hello, World!" << endl; // "end-elle"
+    cout<< "My foo is the mountains." << endl; // "end-elle"
+    return 0;
+}
