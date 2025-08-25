a2c68b72060bb90038bd87ba9105c69eaa2971ed
dayton-will <willd1799@student.faytechcc.edu>
2025-08-25 15:04:25 +0000

    m1t1v2 -- magic wish machine worked properly and greeted user.

---
+++ m1t1v2.cpp
@@ -0,0 +1,22 @@
+/*
+m1t1v2 - hello, user
+CSC 134
+dayton-will, 8/25/25
+INPUT: run the program
+PROCESS: none yet
+OUTPUT: return 0 (no error)
+SPECIAL FX: Greet the user
+m1t1v2*/
+
+//magic words to start -- feed the Magic Wish Machine!
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    //SFX: Greet user
+    cout << "Hello, user!" << endl; // end-elle
+
+    // output
+    return 0; // no errors
+}
