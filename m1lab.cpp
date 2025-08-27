b97030a054b217fe9c4023b851fa61ac885e6f7e
dayton-will <willd1799@student.faytechcc.edu>
2025-08-27 14:33:03 +0000

    Changed name.

--- m1lab.cpp
+++ m1lab.cpp
@@ -13,9 +13,22 @@ number of months for maintenance service, price per month, and the price to prep
 #include <iostream>
 using namespace std;
 int main()  {
+    //Start program.
 //This program will serve as a prototype for pricing prepay for my mobile automotive detailing business.
 //The owner's name
-string name = "Dayton Will"
+string name = "Dayton"
+//Number of months for maintenance package
+int months = 12
+//Price per month
+double pricePerMonth = $100
+
+//calculate the total prepay price
+//TODO
+
+//Print all the information about the business
+
+cout << "Thanks for calling " << name;
+cout << "s mobile detailing." <<endl;
 
 
 
