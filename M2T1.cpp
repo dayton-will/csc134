81c0a879323dc8bdd66cb23ac3c858c8fcd3ab56
dayton-will <willd1799@student.faytechcc.edu>
2025-09-03 15:35:17 +0000

    Refined variables.

--- M2T1.cpp
+++ M2T1.cpp
@@ -1,39 +1,42 @@
 /*
-M2T1 - hello, user
+M2T1 - Tire Shop
 CSC 134
 dayton-will, 9/3/25
 Revision of "sales" program from M1, now with user input.
-m2t1*/
+M2T1*/
 
 //magic words to start -- feed the Magic Wish Machine! (This is the only way I will refer to a computer from now on)
 
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 int main() {
-    // Declare variables ("the truth")
-    string item = "apple";
-    double cost_per = 0.99;
-    int amount = 20;
+    // Declare variables
+    string item = "tire";
+    string items = "tires";
+    double cost_per = 93.50;
+    int amount = 100;
     //Variables for user input
     int amount_purchased;
     double total_cost;
 
-    // Greet the user
-    cout << "Hello! Welcome to our " << item << " store.";
+    // Greet customer
+    cout << "Hello! Welcome to our " << item << " shop.";
     // Ask for information
-    cout << " Each " << item << " costs $" << cost_per << endl;
-    cout << "We have " << amount << " available." << endl;
-    cout << "How many would you like?" << endl;
+    std::cout << " Each " << item << " costs $" << std::fixed << std::setprecision(2) << cost_per << std::endl;
+    cout << "We have " << amount << " " << items << " available." << endl;
+    cout << "How many " << items << " do you need?" << endl;
     // cin: user input into variable
     cin >> amount_purchased;
     // Process information
     total_cost = amount_purchased * cost_per;
     // Output the answer
-    cout << "Your total cost is: $" << amount_purchased << endl;
-
+    //cout << "Your total cost is: $" << total_cost << endl;
+    std::cout << "Your total cost is: $" << std::fixed << std::setprecision(2) << total_cost << std::endl;
+    cout << "Our techs will have those installed within an hour. We accept cash or all major credit cards; no personal checks." << endl;
+    cout << "Thank you for your business. Have a great day!" << endl;
 
     return 0;
-
 
 }
