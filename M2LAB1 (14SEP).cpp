75b64e7f6c2acbd72c8875b964bb5d548a9b8ab3
dayton-will <willd1799@student.faytechcc.edu>
2025-09-08 14:49:32 +0000

    Input variables, initial viability test.

---
+++ M2LAB1 (14SEP).cpp
@@ -0,0 +1,58 @@
+/*
+CSC 134
+M2 Lab 1 - Case Study with Crates
+dayton-will
+9/8/2025
+
+volume =lengthxwidthxheight
+cost= volume x 0.23
+charge= volume x 0.5
+profit = charge-cost
+
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()  {
+//Set up the variables.
+    double length, width, height; // size of crate
+    double volume; //length * width * height;
+    double cost_to_make, charge_to_customer;
+    double profit;
+
+    const double cost_per_cubic_foot = 0.23; //relates to cost
+    const double charge_per_cubic_foot = 0.5; // relates to charge
+
+//Ask the user to input crate info.
+
+cout << "Welcome to ATD Shipping, LLC. We look forward to working with you!" << endl;
+cout << "Crate length:" << endl;
+cin >> length;
+cout << "Crate Width: ";
+cin >> width;
+cout << "Crate Height: ";
+cin >> height;
+
+
+//Calculate cost and prices.
+volume = length * width * height;
+cost_to_make = volume * cost_per_cubic_foot;
+charge_to_customer = volume * charge_per_cubic_foot;
+profit = charge_to_customer - cost_to_make;
+
+//Display results.
+cout << endl;
+cout << "Your crate is " << volume << " cubic feet." << endl;
+cout << "Customer Price: $" << charge_to_customer << endl;
+cout << "Cost to Build : $" << cost_to_make << endl;
+cout << "Profit        : $" << profit << endl;
+
+
+return 0; // no errors
+
+
+
+
+}
