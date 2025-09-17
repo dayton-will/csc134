958e9da61bb158d39f60acd682894c102cb2ccc8
dayton-will <willd1799@student.faytechcc.edu>
2025-09-17 14:32:12 +0000

    Initial Commit

---
+++ M3/M3T1.cpp
@@ -0,0 +1,34 @@
+
+
+#include <iostream>
+using namspace std;
+
+int main() {
+
+// Get infor on the two rectangles
+double length1, length2, width1, width2;
+double area1, area2;
+cout << "Enter the length and width of tow rectangles." << endl;
+cout << "First rectangle." << endl;
+cout << "Length: ";
+cin >> length1;
+cout << "Width: ";
+citn >> width1;
+cout << endl;
+cout << "Second rectangle." << endl;
+cout << "Lenth: ";
+cin >> length2;
+cout << "Width: ";
+cout << "Width: ";
+cin >> width2;
+
+//Calculate the area
+area1 = length1 / length2
+area2 = length2 * width2;
+
+
+//Print the output
+
+return 0; //no errors
+
+}
