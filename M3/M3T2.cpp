0df94d364b69aaa5bc694421f6819da420e18793
dayton-will <willd1799@student.faytechcc.edu>
2025-09-22 15:18:21 +0000

    Added real math (not 'cheating').

--- M3/M3T2.cpp
+++ M3/M3T2.cpp
@@ -4,12 +4,11 @@ CSC 134
 dayton-will
 9/22/25
 Beginning of the craps game
-
 */
 
 #include <iostream>
 #include <cstdlib>
-//#include <time>
+#include <ctime>
 
 using namespace std;
 
@@ -27,15 +26,40 @@ int main() {
     - branch based on win, lose, or point
     - rest comes later/*
     */
-    int roll1 = 2;
-    int roll2 = 5;
+    int roll1;
+    int roll2;
+    int point; // roll if it doesn't immediately win/lose
 
+
+    // Blatantly cheat
+    cout << "enter two dice ( press ENTER between) ";
+    cin >> roll1;
+    cin >> roll2;
+    
+    // Set sum
     int sum = roll1+roll2;
 
-    if (sum == 7) {
+
+    if ( (sum == 7) || (sum == 11) ) {
         cout << "Lucky Seven -- You win!" << endl;
+        cout << "\n" << endl;
+    }
+
+    else if ( ( sum ==2) || (sum == 3) || (sum == 12) ) {
+        cout << "1, 2, 12 -- Sorry, you lose." << endl;
 
     }
+
+    else {
+        // set the point
+        point = sum;
+        cout << "Did not roll a seven." << endl;
+        cout << "Your point is: " << point << endl;
+
+
+    }
+
+    
 
 
 return 0;
@@ -44,9 +68,9 @@ return 0;
 int roll; {
     // rolls a six sided die
     //TODO
-    return 6;
+    return 7;
 }
 
+
+
 }
-
-
