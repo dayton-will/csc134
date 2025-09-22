0f83330acd28959215a5880d6925e54f3767483f
dayton-will <willd1799@student.faytechcc.edu>
2025-09-22 14:50:40 +0000

    Initial commit -- Test build w/ 2 + 5 == 7

---
+++ M3/M3T2.cpp
@@ -0,0 +1,52 @@
+/*
+M3T2 - Craps Part 1
+CSC 134
+dayton-will
+9/22/25
+Beginning of the craps game
+
+*/
+
+#include <iostream>
+#include <cstdlib>
+//#include <time>
+
+using namespace std;
+
+//Helper functions (todo)
+//int roll();
+
+//main
+int main() {
+    // int num = roll();
+    // cout << num << endl;
+    //MAIN CRAPS CYCLR
+    // For now:
+    /*
+    - roll 26 (2-12)
+    - branch based on win, lose, or point
+    - rest comes later/*
+    */
+    int roll1 = 2;
+    int roll2 = 5;
+
+    int sum = roll1+roll2;
+
+    if (sum == 7) {
+        cout << "Lucky Seven -- You win!" << endl;
+
+    }
+
+
+return 0;
+
+//DEFINE Helper Functions
+int roll; {
+    // rolls a six sided die
+    //TODO
+    return 6;
+}
+
+}
+
+
