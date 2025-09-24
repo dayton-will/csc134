2fcde2502e0f96e26839d1c1be6a04f6b5b061f2
dayton-will <willd1799@student.faytechcc.edu>
2025-09-24 14:48:01 +0000

    Created potential customers, defined main function.

--- M3/M3LAB1_v1.cpp
+++ M3/M3LAB1_v1.cpp
@@ -5,3 +5,27 @@ dayton-will
 9/24/2025
 */
 
+include <iostream>
+using namespace std;
+
+//================FUNCTION PROTOTYPES================//
+//Declare all my "rooms" (customers) here
+void Hannah(); // Single mom, messy car. Ford Taurus.
+void Warren(); // Local entrepreneur. Runs a small gym. F250 pickup truck. Has kids and dogs.
+void Lauren(); // Nurse with a "side hustle" as a certified masseuse. Doesn't have kids of her own, but spends a lot of time with nieces/nephews.
+void Jonny(); // Description
+void Lucille(); // Description
+
+void gameOver(); // An ending
+void victory(); // Another ending
+
+//==========MAIN FUNCTION==========//
+int main()
+{
+    cout << "==================================" << endl;
+    cout << "WELCOME TO YOUR ENTREPRENEURIAL ADVENTURE" << endl;
+    cout << "==================================" << endl;
+    cout << endl;
+
+
+}
