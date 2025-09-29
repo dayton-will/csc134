3f89b6820b793319bf24c75e18c9a82b38360254
dayton-will <willd1799@student.faytechcc.edu>
2025-09-29 14:47:07 +0000

    Built "if" statements for grades A-F.

--- M3/M3LAB2.cpp
+++ M3/M3LAB2.cpp
@@ -30,14 +30,40 @@ void letter_grader() {
     // Respond with a letter grade.
     double num_grade;
     string letter_grade;
+    // Constants for grade breakpoints.
+    const double A_GRADE = 90; // (or higher)
+    const double B_GRADE = 80;
+    const double C_GRADE = 70;
+    const double D_GRADE = 60;
+
     cout << "Enter a number grade 0-100: ";
     cin >> num_grade;
 
     // Create the "if" statements.
-    letter_grade = "A";
+    if (num_grade >= A_GRADE ) {
+        letter_grade = "A";
+    }
+
+    else if (num_grade >= B_GRADE ) {
+        letter_grade = "B";
+    }
+
+    else if (num_grade >= C_GRADE ) {
+        letter_grade = "C";
+    }
+
+    else if (num_grade >= D_GRADE ) {
+        letter_grade = "D";
+    }
+
+    else {
+        letter_grade = "F";
+    }
+
+
 
     // Output the answer
-    cout << "A number grade of " << num_grade << "is a(n) " << letter_grade;
+    cout << "A number grade of " << num_grade << " is: " << letter_grade;
     cout << endl << endl;
 
 }
