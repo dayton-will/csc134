3c6d34d76feb9d471b63c5b56c047dac57d3019c
dayton-will <willd1799@student.faytechcc.edu>
2025-09-29 14:29:03 +0000

    Initial Commit

---
+++ M3/M3_Monday.cpp
@@ -0,0 +1,12 @@
+/*
+M3LAB2 - Letter Grades/Game Mechanics Test
+CSC 134
+dayton-will
+9/24/2025
+*/
+
+// words to start -- feed the Magic Wish Machine!
+
+#include <iostream>
+using namespace std;
+
