a9d6c9fd1d55e5704e870194eddab74f965aca8b
dayton-will <willd1799@student.faytechcc.edu>
2025-10-22 14:31:49 +0000

    Initial Commit

---
+++ M4/M4T2/M4HW1.cpp
@@ -0,0 +1,32 @@
+/*
+M4HW1
+CSC 134
+dayton-will
+10/22/2025
+Print Multiplication Table ("Times Table")
+*/
+
+// magic words to start -- feed the Magic Wish Machine!
+
+#include <iostream>
+#include <iomanip> 
+#include <string>
+
+int main() {
+    int input;
+
+    std::cout << "Please type the number 5 to print the 5 times table: ";
+    std::cin >> input;
+
+    if (input == 5) {
+        int i = 1;
+        while (i <= 12) {
+            std::cout << "5 times " << i << " is " << (5 * i) << "." << std::endl;
+            i++;
+        }
+    } else {
+        std::cout << "You did not type 5. Program will exit." << std::endl;
+    }
+
+    return 0;
+}
